bd462948c4b3915e9185f2e6e733eb87f955ea4b
92729 <abcdqbbq@gmail.com>
2020-02-13 21:08:07 +0900

    20.02.13

--- layout/js/main.js
+++ layout/js/main.js
@@ -2,6 +2,14 @@
 $(function(){
     var $sideMenu = $(".main_sd");
     var $menuBtn = $(".main_mn_bt");
+    
+    // skipnav 바로가기
+    $(".skip_nav a").on("keydown", function(e){
+        if($(this).attr("href") == "#sideMenu" && e.keyCode == 13){
+            $(".main_mn_bt").trigger("click");
+            $("#sideMenu").focus();
+        }
+    });
 
     // Menu side Layer
     $menuBtn.on("click", function(e){
